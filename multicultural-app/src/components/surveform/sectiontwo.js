9862187a413851519a63d6592d42b3ff695f1728
MeEtSoNi-lab <meet@tulong.tech>
2024-12-16 16:56:12 -0500

    started second section

--- multicultural-app/src/components/surveform/sectiontwo.js
+++ multicultural-app/src/components/surveform/sectiontwo.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import API_BASE_URL from '../../apiconfig';
+import "./sectiontwo.css"
 
-function sectiontwo() {
+function Sectiontwo() {
+    const [yearsofrecidence,setyearsofrecidence]=useState([]);
+  const [selectedValue, setSelectedValue] = useState('');
+    
+  const handleSelectChange = (e) => {
+    setSelectedValue(e.target.value);
+  };
+    useEffect(() => {
+        const YearsofrecidenceData = async () => {
+          try {
+            const response = await fetch(`${API_BASE_URL}/yearsofrecidence`);
+            if (!response.ok) {
+              throw new Error('Failed to fetch data');
+            }
+            const data = await response.json();
+            setyearsofrecidence(data);
+            console.log(data);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+    
+    
+        YearsofrecidenceData();
+      }, []);
   return (
-    <div>
-        <h1>This is section two</h1>
+    <div className='section-form'>
+    <div className='section-two-form'>
+          <h3>What is your birth year ?</h3>
+          <div className="select-wrapper">
+          <select className='styled-select' value={selectedValue} onChange={handleSelectChange}>
+          {yearsofrecidence.length > 0 ? (
+              yearsofrecidence.map((item, index) => (
+              <option key={index} value={item._id}> {/* Assuming _id is unique */}
+                  {item.years} {/* Assuming 'generation' is the label to display */}
+              </option>
+              ))
+          ) : (
+              <option>No data available</option>
+          )}
+          </select>
+          </div>
+
     </div>
+  </div>
   )
 }
 
-export default sectiontwo
+export default Sectiontwo
