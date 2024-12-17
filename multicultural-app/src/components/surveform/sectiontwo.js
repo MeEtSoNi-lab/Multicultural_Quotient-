93ec8a129032f40b66c36e6e3319e14165d6255a
MeEtSoNi-lab <meet@tulong.tech>
2024-12-17 16:51:18 -0500

    Added remaining questions

--- multicultural-app/src/components/surveform/sectiontwo.js
+++ multicultural-app/src/components/surveform/sectiontwo.js
@@ -6,6 +6,9 @@ function Sectiontwo() {
     const [yearsofrecidence,setyearsofrecidence]=useState([]);
     const [numbercountries,setnumbercountries]=useState([]);
     const [traveledcountries,settraveledcountries]=useState([]);
+    const [languageconverse,setlanguageconverse]=useState([]);
+    const [culturalfood,setculturalfood]=useState([]);
+    const [contentengagement,setcontentengagement]=useState([]);
   const [selectedValue, setSelectedValue] = useState('');
     
   const handleSelectChange = (e) => {
@@ -54,12 +57,57 @@ function Sectiontwo() {
               }
         }
 
+        const languageconverseData=async()=>{
+            try{
+                const response=await fetch(`${API_BASE_URL}/languageconverse`)
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                  }
+                  const data = await response.json();
+                  setlanguageconverse(data);
+                  console.log(data);
+            }catch (error) {
+                console.log(error);
+              }
+        }
 
+
+        const culturalfoodData=async()=>{
+            try{
+                const response=await fetch(`${API_BASE_URL}/culturalfood`)
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                  }
+                  const data = await response.json();
+                  setculturalfood(data);
+                  console.log(data);
+            }catch (error) {
+                console.log(error);
+              }
+        }
+
+
+        const contentengagementData=async()=>{
+            try{
+                const response=await fetch(`${API_BASE_URL}/contentengagement`)
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                  }
+                  const data = await response.json();
+                  setcontentengagement(data);
+                  console.log(data);
+            }catch (error) {
+                console.log(error);
+              }
+        }
     
     
         YearsofrecidenceData();
         NumberofcountrieslivingData();
         travelledcountriesData();
+        languageconverseData();
+        culturalfoodData();
+        contentengagementData();
       }, []);
   return (
     <div className='section-form'>
@@ -104,6 +152,55 @@ function Sectiontwo() {
               traveledcountries.map((item, index) => (
               <option key={index} value={item.score}> {/* Assuming _id is unique */}
                   {item.traveledcountries} {/* Assuming 'generation' is the label to display */}
+              </option>
+              ))
+          ) : (
+              <option>No data available</option>
+          )}
+          </select>
+          </div>
+
+
+          <h3>How many languages can you read, speak or converse in?</h3>
+          <div className="select-wrapper">
+          <select className='styled-select' value={selectedValue} onChange={handleSelectChange}>
+          {languageconverse.length > 0 ? (
+              languageconverse.map((item, index) => (
+              <option key={index} value={item.score}> {/* Assuming _id is unique */}
+                  {item.languagecnverse} {/* Assuming 'generation' is the label to display */}
+              </option>
+              ))
+          ) : (
+              <option>No data available</option>
+          )}
+          </select>
+          </div>
+
+
+
+          <h3> Food is a great way to celebrate and learn about cultures not your own.  How often do you cook or eat food from another culture than your own?</h3>
+          <div className="select-wrapper">
+          <select className='styled-select' value={selectedValue} onChange={handleSelectChange}>
+          {culturalfood.length > 0 ? (
+              culturalfood.map((item, index) => (
+              <option key={index} value={item.score}> {/* Assuming _id is unique */}
+                  {item.culturalfood} {/* Assuming 'generation' is the label to display */}
+              </option>
+              ))
+          ) : (
+              <option>No data available</option>
+          )}
+          </select>
+          </div>
+
+
+          <h3> Do engage with content (such as music, movies, television shows, social media content) that are created from outside your own country of residence or birth?</h3>
+          <div className="select-wrapper">
+          <select className='styled-select' value={selectedValue} onChange={handleSelectChange}>
+          {contentengagement.length > 0 ? (
+              contentengagement.map((item, index) => (
+              <option key={index} value={item.score}> {/* Assuming _id is unique */}
+                  {item.contentengagement} {/* Assuming 'generation' is the label to display */}
               </option>
               ))
           ) : (
