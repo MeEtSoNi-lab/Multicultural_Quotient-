1e683076e8038497a0201230dac5a261b03242f7
MeEtSoNi-lab <meet@tulong.tech>
2024-12-18 15:16:33 -0500

    Added designs in home page

--- multicultural-app/src/components/home/home.js
+++ multicultural-app/src/components/home/home.js
@@ -16,12 +16,12 @@ function Home() {
   return (
     <>
       <div className='home-first-container'>
-        <div className='home-first-container-overlay'>
+      
           <div className='home-content'>
             <h1>What's Your Multicultural Quotient ?</h1>
             <button onClick={handleQuizClick}>TAKE THE QUIZ</button>
           </div>
-        </div>
+        
       </div>
 
       <div className='home-second-container'>
@@ -49,6 +49,15 @@ function Home() {
         </div>
       </div>
 
+        <div className='home-fourth-container'>
+            <div className='home-fourth-container-photo'>
+                <img src="/images/Joycelyn.jpg" alt="" />
+            </div>
+            <div className='home-fourth-container-info'>
+                <h2>Joycelyn David</h2>
+                <p>Working with a creative team and brands that have a positive impact on the lives of Canadians are the best parts of Joycelyn’s role as AVC’s Owner and CEO. A daughter of immigrant parents from the Philippines, Joycelyn understands the hopes and struggles of newcomers and is proud to engage with and help bring multicultural communities together. She thrives on working with clients, exploring ways for them to grow their businesses, expand their reach, and surpass their goals. With a marketing background that spans almost 20 years at global organizations like Western Union and KPMG, Joycelyn’s the ultimate big-picture thinker who brings a uniquely positive, anything’s-possible approach to every new strategic ask. She combines this with a wealth of marketing knowledge and a firm grounding in financials, making her a true force to be reckoned with. She’s also a huge Star Wars fan (we’re talking e-v-e-r-y episode). Adventurer (ask her about her white water rafting or pole jumping exploits). Musician (she can teach you how to play piano). Lover of family gatherings (near and far). Fan of simple foods (plain rice and a fried egg, anyone?). And collector of throw pillows (we’ve got nothing for this one).</p>
+            </div>
+        </div>
       {showSurvey && (
         <div className='survey-popup'>
           <div className='survey-popup-overlay' onClick={handleCloseSurvey}></div>
