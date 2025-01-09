77b102ff4cb0468e43730c33504cd9d715d2c096
MeEtSoNi-lab <meet@tulong.tech>
2025-01-09 11:52:04 -0500

    Changed the workflow for survey form as well as changed in home page design

--- multicultural-app/src/components/home/home.js
+++ multicultural-app/src/components/home/home.js
@@ -9,16 +9,9 @@ import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale } f
 ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale);
 
 function Home() {
-  const { calculatedScore, showSurvey, setShowSurvey } = useContext(multiStepContext);
+  const { calculatedScore} = useContext(multiStepContext);
 
-  const handleQuizClick = () => {
-    setShowSurvey(true);
-  };
-
-  const handleCloseSurvey = () => {
-    setShowSurvey(false);
-  };
-
+ 
   // Data for the Donut chart
   const chartData = {
     labels: ['Score', 'Remaining'],
@@ -49,11 +42,11 @@ function Home() {
       <div className='home-first-container'>
         <div className='home-content'>
           <h1>What's Your Multicultural Quotient ?</h1>
-          <button onClick={handleQuizClick}>TAKE THE QUIZ</button>
+          
         </div>
       </div>
 
-      <div className='home-second-container'>
+      {/* <div className='home-second-container'>
         <h2>"We are living in a multicultural era."</h2>
         <p>- Joycelyn Espiritu David</p>
       </div>
@@ -86,19 +79,13 @@ function Home() {
           <h2>Joycelyn David</h2>
           <p>Working with a creative team and brands that have a positive impact on the lives of Canadians are the best parts of Joycelyn’s role as AVC’s Owner and CEO. A daughter of immigrant parents from the Philippines, Joycelyn understands the hopes and struggles of newcomers and is proud to engage with and help bring multicultural communities together. She thrives on working with clients, exploring ways for them to grow their businesses, expand their reach, and surpass their goals. With a marketing background that spans almost 20 years at global organizations like Western Union and KPMG, Joycelyn’s the ultimate big-picture thinker who brings a uniquely positive, anything’s-possible approach to every new strategic ask. She combines this with a wealth of marketing knowledge and a firm grounding in financials, making her a true force to be reckoned with. She’s also a huge Star Wars fan (we’re talking e-v-e-r-y episode). Adventurer (ask her about her white water rafting or pole jumping exploits). Musician (she can teach you how to play piano). Lover of family gatherings (near and far). Fan of simple foods (plain rice and a fried egg, anyone?). And collector of throw pillows (we’ve got nothing for this one).</p>
         </div>
-      </div>
+      </div> */}
 
-      {showSurvey && (
+      
         <div className='survey-popup'>
-          <div className='survey-popup-overlay' onClick={handleCloseSurvey}></div>
-          <div className='survey-popup-content fade-up'>
-            <button className='close-button' onClick={handleCloseSurvey}>
-              ✖
-            </button>
             <Survey />
-          </div>
         </div>
-      )}
+      
 
       {calculatedScore && (
         <div id='score' className='home-score-container'>
