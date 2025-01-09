b9d5f8455b43337fb953ed46ab24cd6fcc8d9e56
MeEtSoNi-lab <meet@tulong.tech>
2025-01-09 10:40:59 -0500

    changed background multicultural image

--- multicultural-app/src/StepContext.js
+++ multicultural-app/src/StepContext.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{use, useState} from 'react'
 import App from './App'
 export const multiStepContext = React.createContext();
 function StepContext() {
@@ -9,11 +9,40 @@ function StepContext() {
   const [bornData, setBornData] = useState([]);
   const [countryData, setcountryData] = useState([]);
   const [countryResidence,setcountryResidence] =useState([]);
+
   const [selectedbornData,setselectedbornData] =useState('');
   const [selectedcountryData,setselectedcountryData] =useState('');
   const [selectedcountryResidence,setselectedcountryResidence] =useState('');
+
   const [selectedbornDataValue,setselectedbornDataValue]=useState('');
   const [selectedbornDataScore,setselectedbornDataScore]=useState('');
+
+  const [selectedcountryDataValue,setselectedcountryDataValue]=useState('');
+  const [selectedcountryDataScore,setselectedcountryDataScore]=useState('');
+
+  const [selectedcountryResidenceValue,setselectedcountryResidenceValue ] =useState('');
+  const [selectedcountryResidenceScore,setselectedcountryResidenceScore ] =useState('');
+
+  
+  // section two survey states
+
+    // states for fetching the API
+    const [yearsofrecidence,setyearsofrecidence]=useState([]);
+    const [numbercountries,setnumbercountries]=useState([]);
+    const [traveledcountries,settraveledcountries]=useState([]);
+    const [languageconverse,setlanguageconverse]=useState([]);
+    const [culturalfood,setculturalfood]=useState([]);
+    const [contentengagement,setcontentengagement]=useState([]);
+
+    // display and selected data from dropdown
+    const [selectedyearsofrecidence,setselectedyearsofrecidence]=useState([]);
+    const [selectednumbercountries,setselectednumbercountries]=useState([]);
+    const [selectedtraveledcountries,setselectedtraveledcountries]=useState([]);
+    const [selectedlanguageconverse,setselectedlanguageconverse]=useState([]);
+    const [selectedculturalfood,setselectedculturalfood]=useState([]);
+    const [selectedcontentengagement,setselectedcontentengagement]=useState([]);
+
+    
 
 
 
@@ -35,6 +64,33 @@ function StepContext() {
     setselectedcountryResidence(e.target.value);
   };
 
+
+
+  //methodes to set the values for second section form
+
+  const handleyearsofrecidence=(e)=>{
+    setselectedyearsofrecidence(e.target.value);
+  }
+
+  const handlenumbercountries=(e)=>{
+      setselectednumbercountries(e.target.value);
+  }
+
+  const handletraveledcountries=(e)=>{
+      setselectedtraveledcountries(e.target.value);
+  }
+
+  const handlelanguageconverse=(e)=>{
+    setselectedlanguageconverse(e.target.value);
+  }
+
+  const handleculturalfood =(e)=>{
+    setselectedculturalfood(e.target.value);
+  }
+
+  const handlecontentengagement =(e)=>{
+    setselectedcontentengagement(e.target.value);
+  }
 
 
   return (
